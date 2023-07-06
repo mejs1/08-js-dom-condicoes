1900da0ba31f93270dbbf77cf00cadf8ce1fc3fb
Maria Eduarda Jardim Souza <132668414+mejs1@users.noreply.github.com>
2023-07-06 13:30:29 +0000

    Hora do dia

--- exercicio/script.js
+++ exercicio/script.js
@@ -3,15 +3,18 @@ function carregar() {
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
-    msg.innerHTML = 'Agora são ${hora} hora'
+    msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12) {
         // BOM DIA! <3
         img.src = 'fotomanha.png'
+        document.body.style.background = '#febe76'
     } else if (hora >= 12 && hora < 18) {
         // BOA TARDE! <3
-        img.src = 'fototarde.png'
+        img.src = 'fototarde.png' 
+        document.body.style.background = '#905a60'
     } else {
         // BOA NOITE! <3
-        img.src = 'fotonoite.png'
+        img.src = 'fotonoite.png' 
+        document.body.style.background = '#1e3e5a'
     }
 }
